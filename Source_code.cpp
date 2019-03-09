3b1c406ad29297a4cb66a80b1cf22f25264e45b4
Elmatsidis Paul <p.elmatsidis@gmail.com>
2019-03-09 22:31:33 +0200

    Update Source_code.cpp

--- Source_code.cpp
+++ Source_code.cpp
@@ -14,7 +14,7 @@ This utility supplied as FREEWARE.Feel free to use and distribute it.\
 \nHave a nice hack!\n\n\
 Remember: The goal of this program is changing the state of DeepFreeze(Vers.5)\nor(perhaps better)\
  erasing the password!\n\
-As argument use:\n[+] - Thawing\n[-] - Frozening\n[!] - NULLing(Destroing) the password\n\
+As argument use:\n[+] - Thaw\n[-] - Froze\n[!] - NULLing(Destroying) the password\n\
 [?] - Help\n";
 
 const char *ar_pwd="\x72\xC0\xC7\xD1\x40\xE5\xC6\x19\x4F\xAF\x44\x5C\x70\x4A\xAD\xF6\xE3\x74\
@@ -24,18 +24,16 @@ E2\x1F\xCB\x98\xD6\x98\x7C\xFB\x8C\xF5\x88\xB4\x41\x43\x9B\x02\xC1\x5D\x52\x6A\x
 x0A\x6F\x12\xE6\x7B\x14\xDE\x09\x3F\x13\x55\xED\x6A\xA1\xA1\xC2\x85\xAF\x7F\x61\xB7\xE1\x46\xDC\
 \xCA\x17\x8C\xA4\x4A\xC6\x4E\x54\x8A\x87\x36\xFE\x99\xD6\x5F\x2E\x9F\x58\x28\xA9\x6F\xE9\x20";
 
-
- 
 int main(int argc,char* argv[])
 {	
     char ch;
-    if (argc == 1  ||  argc > 2)    // testing for options
+    if (argc == 1  ||  argc > 2) 
 	{ 
 		cerr << "\nNothing for doing...Check the argument! \nType ? for Information" << endl;
 		return 0;  
 	}
 
-    if (argv[1][0]=='?')            // if "?"..print help
+    if (argv[1][0]=='?')            // print help
 	{	
 	cout << help;	
 	return 0;
@@ -47,7 +45,7 @@ int main(int argc,char* argv[])
 		return 0;     }
     fout.seekg(0xC4);            // find position
 	
-	if (argv[1][0]=='!')         // if...delete the password
+	if (argv[1][0]=='!')         // if ok delete the password
 	{
 		cout << "Are you sure in deleting the password? [y/n] ";
         cin >> ch;
@@ -61,19 +59,19 @@ int main(int argc,char* argv[])
 		return 0;
 	}
 
-	if (argv[1][0]=='+')            // if... Thawing
+	if (argv[1][0]=='+')            // Thaw
 	{	
     fout<< char(0x82) << char(0x91) << char (0x34)<< char (0x22);
 	cout << "\nOperation Thawing...Successful!" << endl;
 	exit(1);}
 	
-	if (argv[1][0]=='-')            // if... Frozening
+	if (argv[1][0]=='-')            // Froze
 	{
     fout << char(0x7D) << char(0x6E) << char(0xCB) << char(0xDD);
 	cout << endl << "Operation Frozening...Successful!" << endl;
 	exit(1);
 	}	
 
-	fout.close();                                              // close the file    
-	cout << "\nFailed.No operation has been done..." << endl;  // Just a simple comment...
+	fout.close();                                                
+	cout << "\nFailed.No operation has been done..." << endl;  
 }
